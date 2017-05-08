001f988094f1ef89925e76ffdd829c546cc093c5
frostebite <jas.f.ukcmti@gmail.com>
2017-05-08 16:35:56 +0100

    MessageHandler Readme for info and AuthenticationMessageHandler blocking

---
+++ src/UKSFWebsite.api/Core/HTTPMessageHandler/AuthenticationMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UKSFWebsite.api.Core.HTTPMessageHandler
+{
+    public class AuthenticationMessageHandler : DelegatingHandler
+    {
+        protected async override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            //Debug.WriteLine("Process request");
+            authenticate();
+
+            // Call the inner handler.
+            var response = await base.SendAsync(request, cancellationToken);
+
+            //Debug.WriteLine("Process response");
+
+            return response;
+        }
+
+        private void authenticate()
+        {
+            //this is where authenticatation of requests takes place. 
+        }
+    }
+}
---
+++ src/UKSFWebsite.api/Core/HTTPMessageHandler/Readme.txt
@@ -0,0 +1,11 @@
+﻿#HTTP Message Handlers
+
+##Authentication use
+You can see here:
+https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/authentication-and-authorization-in-aspnet-web-api
+
+In the first section there is mention of setting up custom authentication (the step before authorization). Also linked from this section is the following article:
+https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/http-message-handlers
+
+Which explains how to setup a custom HTTP Message Handler. We use a custom HTTP Message Handler to ensure authentication is always handled before any message reaches a controller. 
+This allows us to use the built in [Authorize] attribute to protect controllers easily and cleanly.
