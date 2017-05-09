e54b2dd9792d87488b6a48d0b9f6b00aa4c0a988
frostebite <jas.f.ukcmti@gmail.com>
2017-05-09 04:16:41 +0100

    Tweaks

--- src/UKSFWebsite.api/Core/Authentication/LoginAttempt.cs
+++ src/UKSFWebsite.api/Core/Authentication/LoginAttempt.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UKSFWebsite.api.Core.Authentication
 {
     public class LoginAttempt
     {
+        private string userid;
+        private string password;
+
+        public bool success { get; private set; }
+        public bool accountExists { get; private set; }
+
+        public LoginAttempt(string userid, string password)
+        {
+            this.userid = userid;
+            this.password = password;
+
+            if (userid == "email" && password == "password")
+                success = true;
+        }
     }
 }
