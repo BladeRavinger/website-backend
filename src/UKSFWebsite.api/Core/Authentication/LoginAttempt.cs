00774a8a3c7ee807cb13ff5555ed382d672e87e1
frostebite <jas.f.ukcmti@gmail.com>
2017-05-10 18:08:35 +0100

    Login logic in login attempt class

--- src/UKSFWebsite.api/Core/Authentication/LoginAttempt.cs
+++ src/UKSFWebsite.api/Core/Authentication/LoginAttempt.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UKSFWebsite.api.Core.Authentication
@@ -13,6 +16,7 @@ namespace UKSFWebsite.api.Core.Authentication
     {
         private string userid;
         private string password;
+        private HttpContext request;
 
         public bool success { get; private set; }
         public bool accountExists { get; private set; }
@@ -22,8 +26,39 @@ namespace UKSFWebsite.api.Core.Authentication
             this.userid = userid;
             this.password = password;
 
-            if (userid == "email" && password == "password")
-                success = true;
+        }
+
+        public LoginAttempt(HttpContext request)
+        {
+            this.request = request;
+        }
+
+        public async Task<AuthenticateResult> TryLogin()
+        {
+            var identity = new ClaimsIdentity("Automatic"); // the name of our auth scheme
+            // you could add any custom claims here
+            var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), null, "Automatic");
+            await request.Authentication.SignInAsync("Automatic", new ClaimsPrincipal(identity));
+            return AuthenticateResult.Success(ticket);
+
+
+            /*StringValues headerValue;
+            if (!Context.Headers.TryGetValue(Options.HeaderName, out headerValue))
+            {
+                return AuthenticateResult.Fail("Missing or malformed 'Authorization' header.");
+            }
+
+            var apiKey = headerValue.First();
+            if (!_validator.ValidateAsync(apiKey))
+            {
+                return AuthenticateResult.Fail("Invalid API key.");
+            }
+
+            // success! Now we just need to create the auth ticket
+            var identity = new ClaimsIdentity("apikey"); // the name of our auth scheme
+            // you could add any custom claims here
+            var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), null, "apikey");
+            return AuthenticateResult.Success(ticket);*/
         }
     }
 }
