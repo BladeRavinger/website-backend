c14f10047c62861a9d42ea51b9129ba7bf9121db
Tim Beswick <contact.tim.here@gmail.com>
2017-05-11 02:16:39 +0100

    Whitespace

--- src/UKSFWebsite.api/Core/Database/Database.cs
+++ src/UKSFWebsite.api/Core/Database/Database.cs
@@ -9,13 +9,13 @@ namespace UKSFWebsite.api.Core.Database
 {
     public class Database
     {
-	    private static IMongoClient databaseConnection;
-		public Database ()
-		{
-			/**
-			 * Setting database connectivity
-			 **/
-			databaseConnection = new MongoClient("mongodb://api:sXH9gjG98wXX@92.222.75.92:27017/UKSF");
+        private static IMongoClient databaseConnection;
+        public Database ()
+        {
+            /**
+             * Setting database connectivity
+             **/
+            databaseConnection = new MongoClient("mongodb://api:sXH9gjG98wXX@92.222.75.92:27017/UKSF");
         }
         public static Database getDatabase()
         {
