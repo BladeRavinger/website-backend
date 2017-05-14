bdf6cc328ba5913aa6cf00c62fe3b8b1f4337e10
frostebite <jas.f.ukcmti@gmail.com>
2017-05-14 21:32:32 +0100

    dotnet publish version

--- runFromSource.py
+++ runFromSource.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import subprocess
+import shutil
+import sys
 
 ## python .\SetupConfigs.py mongourl
 
@@ -13,11 +15,16 @@ def startDotNetDll():
 	subprocess.call(["dotnet", dllpath])
 
 def publishDotNetProgram():
-	subprocess.call(["dotnet", "publish", ".\\src\\UKSFWebsite.api\\", "-o", buildpath])
+	subprocess.call(["dotnet", "build", ".\\src\\UKSFWebsite.api\\", "-o", buildpath, "--framework", "netcoreapp1.1", "--runtime", sys.argv[1]])
 
 def getGitConfigFolder():
 	subprocess.call(["git", "clone", "https://github.com/uksf/website-backend-config.git", buildpath+path])
 
+def clearOldBuild():
+	if(os.path.isdir(buildpath)):
+		shutil.rmtree(buildpath)
+
+clearOldBuild()
 publishDotNetProgram()
 getGitConfigFolder()
 startDotNetDll()
