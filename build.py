9d7d8d4a69153d12edf1a4080c74c284a9ce68f5
frostebite <jas.f.ukcmti@gmail.com>
2017-05-15 03:17:41 +0100

    added coverals.io and subprocess exit code being used in build.py

--- build.py
+++ build.py
@@ -9,4 +9,8 @@ dllpath = "./UKSFWebsite.api.dll"
 
 print("Build script starting in path - " + os.getcwd())
 
-subprocess.call(["dotnet", "publish", os.getcwd() + sourcepath, "-o", buildpath, "--framework", "netcoreapp1.0", "--runtime", sys.argv[1]])
+
+try:
+   grepOut = subprocess.call(["dotnet", "publish", os.getcwd() + sourcepath, "-o", buildpath, "--framework", "netcoreapp1.0", "--runtime", sys.argv[1]], shell=True)                      
+except subprocess.CalledProcessError as grepexc:                                                                                                   
+    print "error code", grepexc.returncode, grepexc.output
