b02cdd34eeae172cd42e3bdfc9a5e2b2b4be6aa2
frostebite <jas.f.ukcmti@gmail.com>
2017-05-15 03:22:58 +0100

    using check_ouput in build.py

--- build.py
+++ build.py
@@ -11,6 +11,6 @@ print("Build script starting in path - " + os.getcwd())
 
 
 try:
-   grepOut = subprocess.call(["dotnet", "publish", os.getcwd() + sourcepath, "-o", buildpath, "--framework", "netcoreapp1.0", "--runtime", sys.argv[1]], shell=True)                      
+   grepOut = subprocess.check_output(["dotnet", "publish", os.getcwd() + sourcepath, "-o", buildpath, "--framework", "netcoreapp1.0", "--runtime", sys.argv[1]], shell=True)                      
 except subprocess.CalledProcessError as grepexc:                                                                                                   
     print "error code", grepexc.returncode, grepexc.output
