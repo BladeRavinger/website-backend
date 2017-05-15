4c17f7cb65775a97a23a50293b7a7283229af9ac
frostebite <jas.f.ukcmti@gmail.com>
2017-05-15 02:58:28 +0100

    \\ to / in build.py

--- build.py
+++ build.py
@@ -2,11 +2,11 @@ import subprocess
 import sys
 import os
 
-sourcepath = "\\src\\UKSFWebsite.api"
-buildpath = ".\\build_output"
-path = "\\website-backend-config"
-dllpath = ".\\UKSFWebsite.api.dll"
+sourcepath = "/src/UKSFWebsite.api/"
+buildpath = "./build_output"
+path = "/website-backend-config"
+dllpath = "./UKSFWebsite.api.dll"
 
-print(os.getcwd())
+print("Build script starting in path - " + os.getcwd())
 
 subprocess.call(["dotnet", "publish", os.getcwd() + sourcepath, "-o", buildpath, "--framework", "netcoreapp1.0", "--runtime", sys.argv[1]])
