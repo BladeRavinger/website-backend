8b058fab4c33f12283edf225750ed91f38d7b9b5
markusd <jas.f.ukcmti>
2017-05-16 14:12:42 +0100

    Functioning Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 # Use an official Python runtime as a base image
-FROM microsoft/aspnetcore
+FROM microsoft/dotnet:1.0-runtime
 
 # Set the working directory to /app
 WORKDIR /app
 
 # Copy the current directory contents into the container at /app
-ADD /build_output /app
-
-EXPOSE 80
-EXPOSE 5000
+COPY /build_output /app
+COPY /build_output/website-backend-config /app/website-backend-config
 
 ENTRYPOINT ["dotnet", "UKSFWebsite.api.dll"]
--- src/UKSFWebsite.api/Core/SensitiveConfigurationManager.cs
+++ src/UKSFWebsite.api/Core/SensitiveConfigurationManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace UKSFWebsite.api.Core
 {
@@ -13,7 +14,7 @@ namespace UKSFWebsite.api.Core
         internal static void Setup(IConfigurationRoot configuration)
         {
             //needs to read dbConUrl from configuration
-            dbConUrl = System.IO.File.ReadAllText(".\\website-backend-config\\database.json"); ;
+            dbConUrl = System.IO.File.ReadAllText(Path.Combine(".", "website-backend-config", "database.json")); ;
         }
     }
 }
