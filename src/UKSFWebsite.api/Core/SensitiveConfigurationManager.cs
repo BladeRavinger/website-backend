6ecbc02febf1e24ca39152aad314036d847643e1
frostebite <jas.f.ukcmti@gmail.com>
2017-05-18 23:16:11 +0100

    use environment variable if config not found

--- src/UKSFWebsite.api/Core/SensitiveConfigurationManager.cs
+++ src/UKSFWebsite.api/Core/SensitiveConfigurationManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -14,7 +15,13 @@ namespace UKSFWebsite.api.Core
         internal static void Setup(IConfigurationRoot configuration)
         {
             //needs to read dbConUrl from configuration
-            dbConUrl = System.IO.File.ReadAllText(Path.Combine(".", "website-backend-config", "database.json")); ;
+            if (File.Exists(Path.Combine(".", "website-backend-config", "database.json")))
+                dbConUrl = File.ReadAllText(Path.Combine(".", "website-backend-config", "database.json"));
+            else
+            {
+                Trace.TraceInformation("Private config not found attempting to use environment variable");
+                dbConUrl = Environment.GetEnvironmentVariable("dbConUrl");
+            }
         }
     }
 }
