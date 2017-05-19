872535feaa9086be3c7120066c3edaff93672a21
frostebite <jas.f.ukcmti@gmail.com>
2017-05-19 01:04:55 +0100

    braces consistent in SensitiveConfigurationManager

--- src/UKSFWebsite.api/Core/SensitiveConfigurationManager.cs
+++ src/UKSFWebsite.api/Core/SensitiveConfigurationManager.cs
@@ -15,8 +15,11 @@ namespace UKSFWebsite.api.Core
         internal static void Setup(IConfigurationRoot configuration)
         {
             //needs to read dbConUrl from configuration
-            if (Directory.Exists(Path.Combine(".", "website-backend-config")) && File.Exists(Path.Combine(".", "website-backend-config", "database.json")))
+            if (Directory.Exists(Path.Combine(".", "website-backend-config")) &&
+                File.Exists(Path.Combine(".", "website-backend-config", "database.json")))
+            {
                 dbConUrl = File.ReadAllText(Path.Combine(".", "website-backend-config", "database.json"));
+            }
             else
             {
                 Trace.TraceInformation("Private config not found attempting to use environment variable");
