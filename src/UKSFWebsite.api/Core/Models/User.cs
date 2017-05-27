8362fb8a0611daee7630ca807cb70efe61ebcf06
frostebite <jas.f.ukcmti@gmail.com>
2017-05-27 20:38:32 +0100

    cleanup

--- src/UKSFWebsite.api/Core/Models/User.cs
+++ src/UKSFWebsite.api/Core/Models/User.cs
@@ -1,17 +1,14 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 
 namespace UKSFWebsite.api.Core.Models
 {
     public class User
     {
-        [BsonId]
-		public object id { get; set; } //MongoDb uses this field as identity.
+		public ObjectId id { get; set; } //MongoDb uses this field as identity.
 
-		[BsonRepresentation(MongoDB.Bson.BsonType.String)]
-		public string username { get; set; }
-
-		[BsonRepresentation(MongoDB.Bson.BsonType.String)]
-		public string password { get; set;}
+        public string username;
+        public string password;
     }
 }
