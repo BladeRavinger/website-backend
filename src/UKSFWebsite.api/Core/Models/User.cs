5c9f115a3e1f12f06173a037e96713952e52cbf7
frostebite <jas.f.ukcmti@gmail.com>
2017-05-27 20:28:00 +0100

    Using BSONId

--- src/UKSFWebsite.api/Core/Models/User.cs
+++ src/UKSFWebsite.api/Core/Models/User.cs
@@ -5,7 +5,8 @@ namespace UKSFWebsite.api.Core.Models
 {
     public class User
     {
-		public object _id { get; set; } //MongoDb uses this field as identity.
+        [BsonId]
+		public object id { get; set; } //MongoDb uses this field as identity.
 
 		[BsonRepresentation(MongoDB.Bson.BsonType.String)]
 		public string username { get; set; }
