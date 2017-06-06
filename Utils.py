602e3cf402affc4675c1ce95703b6f51098a5ef5
frostebite <jas.f.ukcmti@gmail.com>
2017-06-06 12:58:38 +0100

    print result directly ignoring length

--- Utils.py
+++ Utils.py
@@ -107,7 +107,5 @@ def runSSHCommand(client, command):
 	while not stdout.channel.exit_status_ready():
 		# Only print data if there is data to read in the channel
 		if stdout.channel.recv_ready():
-			rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
-			if len(rl) > 0:
-				# Print data from stdout
-				print stdout.channel.recv(1024),
+			# Print data from stdout
+			print stdout.channel.recv(1024)
