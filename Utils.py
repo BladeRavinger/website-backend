6a55d22e6751b966b7a9ca112090bf783a46d53c
frostebite <jas.f.ukcmti@gmail.com>
2017-06-06 12:50:53 +0100

    running multiple ssh commands

--- Utils.py
+++ Utils.py
@@ -97,14 +97,20 @@ def SSHandDeploy(VPS_HOSTNAME):
 		client.load_system_host_keys()
 		
 		client.connect(hostname, port=port, username=username, password=password)
-
-		stdin, stdout, stderr = client.exec_command("sudo ls")
-		while not stdout.channel.exit_status_ready():
-			# Only print data if there is data to read in the channel
-			if stdout.channel.recv_ready():
-				rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
-				if len(rl) > 0:
-					# Print data from stdout
-					print stdout.channel.recv(1024),
+		runSSHCommand(client, "cd ..")
+		runSSHCommand(client, "ls")
+		runSSHCommand(client, "docker -v")
+		
 	finally:
 		client.close()
+		
+def runSSHCommand(client, command):
+	stdin, stdout, stderr = client.exec_command("sudo "+command)
+	
+	while not stdout.channel.exit_status_ready():
+		# Only print data if there is data to read in the channel
+		if stdout.channel.recv_ready():
+			rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
+			if len(rl) > 0:
+				# Print data from stdout
+				print stdout.channel.recv(1024),
