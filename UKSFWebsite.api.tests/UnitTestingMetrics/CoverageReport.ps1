8bec8b5c2f1a249c6a13f26d427555787aaf90c0
frostebite <jas.f.ukcmti@gmail.com>
2017-06-08 22:07:59 +0100

    removed filter from code coverage

--- UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
+++ UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
@@ -2,5 +2,5 @@
 nuget install .\UKSFWebsite.api.tests\packages.config -outputDirectory .\packages;
 dotnet publish .\UKSFWebsite.api.tests -o .\build_tests --framework net451 --runtime any;
 ls;
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"build_tests\UKSFWebsite.api.tests.dll" -filter:"+[UKSFWebsite.api]UKSFWebsite.api*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml";
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"build_tests\UKSFWebsite.api.tests.dll" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml";
 .\packages\ReportGenerator.2.5.8\tools\ReportGenerator.exe "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport"
