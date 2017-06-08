ad8e138d89b000fee2217280902bdc887443016d
frostebite <jas.f.ukcmti@gmail.com>
2017-06-08 21:38:10 +0100

    moved coveralls report to appveyor

--- UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
+++ UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
@@ -4,5 +4,3 @@ dotnet publish .\UKSFWebsite.api.tests -o .\build_tests --framework net451 --run
 ls;
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"build_tests\UKSFWebsite.api.tests.dll" -filter:"+[UKSFWebsite.api]UKSFWebsite.api*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml";
 .\packages\ReportGenerator.2.5.8\tools\ReportGenerator.exe "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport"
-
-.\packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe  --opencover -i .\_CodeCoverageResult.xml --useRelativePaths  
