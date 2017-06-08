228d97c6a81777da99a4d3c6890d17412f0d28c0
frostebite <jas.f.ukcmti@gmail.com>
2017-06-08 20:28:09 +0100

    do not need relative path

--- UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
+++ UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
@@ -1,8 +1,8 @@
-﻿python ..\..\Build.py --onlyBuild any;
-dotnet build ..\..\UKSFWebsite.api.tests;
-dotnet test ..\..\UKSFWebsite.api.tests;
-nuget install ..\..\UKSFWebsite.api.tests\packages.config -outputDirectory .\packages;
-dotnet publish ..\..\UKSFWebsite.api.tests -o ..\..\build_tests --framework net451 --runtime any;
+﻿python .\Build.py --onlyBuild any;
+dotnet build .\UKSFWebsite.api.tests;
+dotnet test .\UKSFWebsite.api.tests;
+nuget install .\UKSFWebsite.api.tests\packages.config -outputDirectory .\packages;
+dotnet publish .\UKSFWebsite.api.tests -o .\build_tests --framework net451 --runtime any;
 ls;
-..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"..\..\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"..\..\build_tests\UKSFWebsite.api.tests.dll" -filter:"+[UKSFWebsite.api]UKSFWebsite.api*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"..\..\_CodeCoverageResult.xml";
-..\..\packages\ReportGenerator.2.5.8\tools\ReportGenerator.exe "-reports:..\..\_CodeCoverageResult.xml" "-targetdir:..\..\_CodeCoverageReport"
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"build_tests\UKSFWebsite.api.tests.dll" -filter:"+[UKSFWebsite.api]UKSFWebsite.api*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml";
+.\packages\ReportGenerator.2.5.8\tools\ReportGenerator.exe "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport"
