dbd772b8df63c35d2dd67177a00bdb90c026b2a6
frostebite <jas.f.ukcmti@gmail.com>
2017-06-08 21:03:18 +0100

    Coveralls tools path specified

--- UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
+++ UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
@@ -5,4 +5,4 @@ ls;
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"build_tests\UKSFWebsite.api.tests.dll" -filter:"+[UKSFWebsite.api]UKSFWebsite.api*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml";
 .\packages\ReportGenerator.2.5.8\tools\ReportGenerator.exe "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport"
 
-csmacnz.Coveralls.exe --opencover -i .\CodeCoverageResult.xml --useRelativePaths  
+.\packages\coveralls.net.0.7.0\tools\Coveralls.exe --opencover -i .\CodeCoverageResult.xml --useRelativePaths  
