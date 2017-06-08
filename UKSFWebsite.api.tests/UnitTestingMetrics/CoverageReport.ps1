a9a421e2d61e4b00aa03943d9611b8b5e10e3f17
frostebite <jas.f.ukcmti@gmail.com>
2017-06-08 20:25:58 +0100

    all / to \

--- UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
+++ UKSFWebsite.api.tests/UnitTestingMetrics/CoverageReport.ps1
@@ -1,7 +1,7 @@
-﻿python ../../Build.py --onlyBuild any;
-dotnet build ../../UKSFWebsite.api.tests;
-dotnet test ../../UKSFWebsite.api.tests;
-nuget install ../../UKSFWebsite.api.tests/packages.config -outputDirectory ./packages;
+﻿python ..\..\Build.py --onlyBuild any;
+dotnet build ..\..\UKSFWebsite.api.tests;
+dotnet test ..\..\UKSFWebsite.api.tests;
+nuget install ..\..\UKSFWebsite.api.tests\packages.config -outputDirectory .\packages;
 dotnet publish ..\..\UKSFWebsite.api.tests -o ..\..\build_tests --framework net451 --runtime any;
 ls;
 ..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"..\..\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"..\..\build_tests\UKSFWebsite.api.tests.dll" -filter:"+[UKSFWebsite.api]UKSFWebsite.api*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"..\..\_CodeCoverageResult.xml";
