9ba978b9aa1d2f434e8f715d88691c4c7f6bee9b
frostebite <jas.f.ukcmti@gmail.com>
2017-06-14 00:34:36 +0100

    Response time

--- UKSFWebsite.api/Middleware/ResponseTimeMiddleware.cs
+++ UKSFWebsite.api/Middleware/ResponseTimeMiddleware.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace UKSFWebsite.api.Middleware
@@ -12,9 +15,26 @@ namespace UKSFWebsite.api.Middleware
             this.next = next;
         }
 
-        public Task Invoke(HttpContext context)
+        public async Task Invoke(HttpContext context)
         {
-            return this.next(context);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            context.Response.OnStarting(() =>
+            {
+                stopwatch.Stop();
+                context.Response.Headers["ResponseTime"] = stopwatch.ElapsedMilliseconds.ToString();
+                return Task.CompletedTask;
+            });
+            await next(context);
+            stopwatch.Stop();
+        }
+    }
+    public static class ResponseTimeMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseResponseTime(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ResponseTimeMiddleware>();
         }
     }
 }
