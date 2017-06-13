f6629aee0bfc0798a597452682c41693ae3a203b
frostebite <jas.f.ukcmti@gmail.com>
2017-06-13 23:45:25 +0100

    test middleware

---
+++ UKSFWebsite.api/Middleware/ResponseTimeMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace UKSFWebsite.api.Middleware
+{
+    public class ResponseTimeMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ResponseTimeMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            return this.next(context);
+        }
+    }
+}
