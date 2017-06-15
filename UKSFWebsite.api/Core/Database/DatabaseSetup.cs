1d0a63bcd56edace1bc0df9503edf436bd440777
frostebite <jas.f.ukcmti@gmail.com>
2017-06-15 02:36:05 +0100

    stubbed DatabaseSetup

---
+++ UKSFWebsite.api/Core/Database/DatabaseSetup.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UKSFWebsite.api.Core.Database
+{
+    public class DatabaseSetup
+    {
+        private IMongoClient dbServer;
+        public DatabaseSetup()
+        {
+            setupRequiredDatabases();
+            setupRequiredCollections();
+            //setupRequiredDefaultValues();//TODO
+        }
+
+        private void setupRequiredDatabases()
+        {
+            //loop
+            //if doesn't exist then setupDatabase()
+        }
+
+        private void setupRequiredCollections()
+        {
+            //same as db
+        }
+
+        private void setupCollection()
+        {
+
+        }
+
+        private void setupDatabase()
+        {
+
+        }
+
+        private bool checkDatabase()
+        {
+            return false;
+        }
+
+        private bool checkCollection()
+        {
+            return false;
+        }
+    }
+}
